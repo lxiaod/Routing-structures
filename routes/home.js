5cb8bd6f3857cdbd0e0496202e2c7e84933d1ef9
liudong <985569132@qq.com>
2017-10-30 20:50:58 +0800

    路由的处理函数

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,34 @@
 /**
  * Created by Administrator on 2017/10/28.
  */
+//首页的处理函数
 exports.index = (req,res,next)=>{
-    res.send('hello world');
+    res.render('index',{
+        title:'首页'
+    })
 }
+//注册页面的处理函数
+exports.register = (req,res,next)=>{
+    res.render('register',{
+        title:'注册页面'
+    })
+}
+//登录页面的处理函数
+exports.login = (req,res,next)=>{
+    res.render('login',{
+        title:'登录页面'
+    })
+}
+//注册行为的处理函数
+exports.postRegister = (req,res,next)=>{
+
+}
+//登录行为的处理函数
+exports.postLogin = (req,res,next)=>{
+
+}
+//退出行为的处理函数
+exports.logout = (req,res,next)=>{
+
+}
+
