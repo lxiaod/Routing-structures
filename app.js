f443f6727a5fad45b092f1191fdb3a9b9797d18f
liudong <985569132@qq.com>
2017-11-06 11:43:28 +0800

    基本静态页面完成

--- app.js
+++ app.js
@@ -19,27 +19,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
-
 // catch 404 and forward to error handler
 app.use((req, res, next) =>{
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
 // error handler
 app.use((err, req, res, next)=>{
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(3000,()=>{
+app.listen(5000,()=>{
     console.log('ok');
 })
 module.exports = app;
